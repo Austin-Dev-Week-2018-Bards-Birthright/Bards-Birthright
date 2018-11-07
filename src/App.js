dd56bf7b4653649456b9d933d9b2a8e5aa2a737f
Mitchel Constantin <mitchconstantin@gmail.com>
2018-11-07 09:39:12 -0600

    add support for dotenv

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
+var DBURL = process.env.REACT_APP_DB_URL;
+var REVKEY = process.env.REACT_APP_REV_API_KEY;
 
+console.log('your rev key is: ', REVKEY);
+console.log('your DBURL is: ', DBURL);
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload. Here is your key:
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
-      </div>
-    );
+      </div>;
   }
 }
 
