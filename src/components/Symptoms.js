aec077d849fe8045dc8a8e233891a5166c78745b
Mitchel Constantin <mitchconstantin@gmail.com>
2018-11-07 11:43:59 -0600

    display symptoms from dictionary

--- src/components/Symptoms.js
+++ src/components/Symptoms.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { input, output } from '../sampleDataTranscriptionPretty.js';
+
+let SYMPTOM_DICTIONARY = {
+  snake: 'snake',
+  breakfast: 'breakfast',
+  downtown: 'downtown'
+};
+
+let SYMPTOMS = []
+
+input.monologues.forEach(speaker => {
+  speaker.elements.forEach(textChunk => {
+    SYMPTOMS.push(SYMPTOM_DICTIONARY[textChunk.value]);
+  });
+});
 
 class Symptoms extends Component {
   render() {
-    return (<div className="Symptoms">
-     <p>I am a Symptoms</p>
-    </div>);
+    return (
+      <div className="Symptoms">
+      <p>Here is your list of symptoms we found: </p>
+        {SYMPTOMS.map((item, i) => {
+          return <p key={i}> {item} </p>;
+        })}
+      </div>
+    );
   }
 }
 
