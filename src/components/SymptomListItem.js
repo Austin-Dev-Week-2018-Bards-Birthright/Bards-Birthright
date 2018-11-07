8080abad41784960703a8dfd8275f2fa87f52548
Mitchel Constantin <mitchconstantin@gmail.com>
2018-11-07 13:32:39 -0600

    allow user to edit symptoms

--- src/components/SymptomListItem.js
+++ src/components/SymptomListItem.js
@@ -16,20 +16,42 @@ let CONFIDENCE_DICTIONARY = {
 class SymptomListItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      symptom : this.props.symptom,
+      isEditing: false
+    }
     this.save = function () {
-      console.log('wow you saved! ', this.props.symptom.value);
+      this.setState({ symptom: { ...this.state.symptom, confidence: 1 } }) 
     };
     this.edit = function () {
-      console.log('wow you edited!', this.props.symptom.value);
+      this.setState({
+        isEditing: true
+      })
     };
+
+    this.changeText = function (e) {
+      this.setState({ symptom: { ...this.state.symptom, value: e.target.value, confidence: 1 }}) 
+    }
     this.save = this.save.bind(this);
     this.edit = this.edit.bind(this);
+    this.changeText = this.changeText.bind(this);
   }
 
   render() {
-    return <div>     
-      <b style={{ color: CONFIDENCE_DICTIONARY[Math.floor(this.props.symptom.confidence * 10)]}}>{this.props.symptom.value} </b> <button onClick={this.save}>save</button> <button onClick={this.edit}>edit</button>
-      </div>;
+    return (
+    this.state.isEditing ?
+    <div>
+          <input type="text" value={this.state.symptom.value} onChange={this.changeText} />
+          <button onClick={() => this.setState({isEditing: false})}>Confirm</button> 
+    </div>
+        :
+    <div>     
+      <b style={{ color: CONFIDENCE_DICTIONARY[Math.floor(this.state.symptom.confidence * 10)]}}>{this.state.symptom.value} </b> 
+      <button onClick={this.save}>save</button> 
+      <button onClick={this.edit}>edit</button>
+      </div>
+      
+      )
   }
 }
 
