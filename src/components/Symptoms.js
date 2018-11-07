ec827cc517f52afc61cd26d45a30fe9e4091471e
Mitchel Constantin <mitchconstantin@gmail.com>
2018-11-07 11:58:52 -0600

    make symptoms a class

--- src/components/Symptoms.js
+++ src/components/Symptoms.js
@@ -2,26 +2,35 @@ import React, { Component } from 'react';
 import '../App.css';
 import { input, output } from '../sampleDataTranscriptionPretty.js';
 
-let SYMPTOM_DICTIONARY = {
-  snake: 'snake',
-  breakfast: 'breakfast',
-  downtown: 'downtown'
-};
-
-let SYMPTOMS = []
-
-input.monologues.forEach(speaker => {
-  speaker.elements.forEach(textChunk => {
-    SYMPTOMS.push(SYMPTOM_DICTIONARY[textChunk.value]);
-  });
-});
-
 class Symptoms extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      symptoms : [],
+      SYMPTOM_DICTIONARY: {
+        snake: 'snake',
+        breakfast: 'breakfast',
+        downtown: 'downtown'
+      }
+    };
+}
+componentDidMount() {
+  let symptoms = [];
+  input.monologues.forEach(speaker => {
+    speaker.elements.forEach(textChunk => {
+      symptoms.push(this.state.SYMPTOM_DICTIONARY[textChunk.value]); 
+    });
+  }) 
+  this.setState({symptoms})
+}
+
   render() {
+    console.log(this.state.SYMPTOMS);
     return (
       <div className="Symptoms">
       <p>Here is your list of symptoms we found: </p>
-        {SYMPTOMS.map((item, i) => {
+        {this.state.symptoms.map((item, i) => {
           return <p key={i}> {item} </p>;
         })}
       </div>
