dc2e7c6bd28a06d76cdc39bfe5af53fbd764aefa
Michael Thomasson <mjthomasson@gmail.com>
2018-11-07 22:07:31 -0600

    minor syntax change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ const Navbar = (props) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link" href={`#`} onClick={() => props.changePage('recorder')}>Recorder</a>
-          <a className="nav-item nav-link" href={`#`} onClick={() => props.changePage('transcript')}>Transcript</a>
+          <a className="nav-item nav-link" href="#" onClick={() => props.changePage('recorder')}>Recorder</a>
+          <a className="nav-item nav-link" href="#" onClick={() => props.changePage('transcript')}>Transcript</a>
         </div>
       </div>
     </nav>
