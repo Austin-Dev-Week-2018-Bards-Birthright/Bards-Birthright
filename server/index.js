3e3dbd4cefd6893808f658a41e0ba8a57218b0f6
Jacob Hood <jacobwhood@gmail.com>
2018-11-07 19:14:07 -0600

    Feat/send recording to rev.ai api (#21)
    * Update gitignore
    
    * Add request and uuid dependencies
    
    * Completed audio file persistence and uploading
    
    * Delete unused endpoint

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const axios = require('axios');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+const request = require('request');
+const uuidv1 = require('uuid/v1');
 
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
@@ -13,7 +15,11 @@ app.use(cors());
 app.use(express.static(__dirname + '../public/'));
 
 // in memory store of completed transcripts
-let completedTranscriptJobs = {};
+let cache = {
+  completedTranscriptJobs: { },
+  prescriptions: { },
+  symptoms: { }
+};
 
 /**
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -31,15 +37,22 @@ const loadCompletedTranscriptJobIntoMemory = (transcriptJobId) => {
   axios.get(`${process.env.REV_BASE_URL}/jobs/${transcriptJobId}/transcript`, { headers: headers })
     .then(transcript => {
       console.log(transcript.data.monologues);
+      console.log('cache: ', cache);
+      cache.completedTranscriptJobs[transcriptJobId] = transcript.data.monologues;
     })
     .catch(console.log);
 };
 
-const stopPollingInterval = (transcriptJobId) => {
+const stopPollingInterval = (transcriptJobId, transcriptJobStatus) => {
   try {
-    console.log('clearing polling interval');
-    clearInterval(intervalIds[transcriptJobId]);
-    loadCompletedTranscriptJobIntoMemory(transcriptJobId);
+    if (transcriptJobStatus === 'transcribed') {
+      console.log('clearing polling interval');
+      clearInterval(intervalIds[transcriptJobId]);
+      loadCompletedTranscriptJobIntoMemory(transcriptJobId);
+    } else { // failure
+      console.log('clearing polling interval');
+      clearInterval(intervalIds[transcriptJobId]);
+    }
   } catch (err) {
     console.log('error clearing interval: ', err);
     return err;
@@ -52,8 +65,14 @@ const monitorTranscriptionJob = (transcriptJobId) => {
   };
 
   axios.get(`${process.env.REV_BASE_URL}/jobs/${transcriptJobId}`, { headers: headers })
-    .then((transcriptJob) => {
-      if (transcriptJob.data.status === 'transcribed') stopPollingInterval(transcriptJobId);
+    .then(( transcriptJob ) => {
+      let currentTranscriptJobStatus = transcriptJob.data.status;
+      console.log('polling job status: ', currentTranscriptJobStatus);
+      if (currentTranscriptJobStatus === 'transcribed') stopPollingInterval(transcriptJobId, currentTranscriptJobStatus);
+      if (currentTranscriptJobStatus === 'failed') {
+        console.log('failed transcription job: ');
+        stopPollingInterval(transcriptJobId, currentTranscriptJobStatus);
+      }
       return false;
     })
     .catch(console.log);
@@ -71,23 +90,50 @@ const startTranscriptionJobPolling = (transcriptJobId) => {
  */
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
-// Submit audio file for transcription
-app.post('/api/transcribe', (req, res) => {
-  let { file_url } = req.body;
-  let headers = {
-    'Authorization': `Bearer ${process.env.REV_API_KEY}`
-  };
+// Submit audio file for transcription --> REMOTE FILE
+// app.post('/api/transcribe', (req, res) => {
+//   let { file_url } = req.body;
+//   let headers = {
+//     'Authorization': `Bearer ${process.env.REV_API_KEY}`
+//   };
 
-  axios.post(`${process.env.REV_BASE_URL}/jobs`, { 'Content-Type': 'application/json', 'media_url': file_url }, { headers: headers })
-    .then(({ data }) => {
-      console.log('job submitted: ', data);
-      startTranscriptionJobPolling(data.id);
-      res.send('job submitted successfully').status(200);
-    })
-    .catch(console.log);
+//   axios.post(`${process.env.REV_BASE_URL}/jobs`, { 'Content-Type': 'application/json', 'media_url': file_url }, { headers: headers })
+//     .then( ({ data }) => {
+//       console.log('job submitted: ', data);
+//       startTranscriptionJobPolling(data.id);
+//       res.send('job submitted successfully').status(200);
+//     })
+//     .catch(console.log);
+// });
+
+// serve local file to Rev API
+app.post('/api/transcribe', bodyParser.raw({ limit: '50mb' }), (req, res) => {
+  let file_url = path.join(__dirname, `/../public/audio-${uuidv1().split('-')[0]}.mp4`);
+
+  fs.writeFile(file_url, req.body, (err) => {
+    if (err) throw err;
+
+    let config = {
+      url: `${process.env.REV_BASE_URL}/jobs`,
+      formData: { media: fs.createReadStream(file_url), type: 'audio/mp4' },
+      headers: {
+        'Authorization': `Bearer ${process.env.REV_API_KEY}`,
+        'Content-Type': 'multipart/form'
+      }
+    };
+
+    request.post(config, (err, result) => {
+      if (err) console.log(err);
+      else {
+        console.log('job submitted: ', JSON.parse(result.body).id);
+        startTranscriptionJobPolling(JSON.parse(result.body).id);
+        res.send(file_url).status(200);
+      }
+    });
+  });
 });
 
 // retrieve completed transcript for transcript_id
@@ -96,14 +142,6 @@ app.get('/api/retrieve-transcript', (req, res) => {
 
   // res.send(transcripts[id]).status(200);
 });
-
-app.post('/api/audio', bodyParser.raw({ limit: '50mb', extended: true }), (req, res) => {
-  fs.writeFile('./public/currentAudio.mp3', req.body, (err) => {
-    if (err) throw err;
-    res.send("Success");
-    console.log('The audio file has been saved!');
-  })
-})
 
 let port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`server listening on port ${port}`));
